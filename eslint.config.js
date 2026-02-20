0aec01bc93e7ea3b0c1d0f3cd360c98a654ba144
Bharath <vjbharath2002@gmail.com>
2026-02-20 22:54:43 +0530

    feat: Prettier setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,34 @@
 // @ts-check
 // https://typescript-eslint.io/getting-started/typed-linting
 
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      // TypeScript specific
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: './tsconfig.json'
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        },
+        rules: {
+            // TypeScript specific
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
 
-      // General rules
-      eqeqeq: ['error', 'always'],
-      semi: ['error', 'always'],
+            // General rules
+            eqeqeq: ['error', 'always'],
+            semi: ['error', 'always'],
 
-      "no-console" : "error"
-    },
-  },
-];
+            'no-console': 'error'
+        }
+    }
+]
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
 // console.log("app.ts");
+const a = 10
+if (a) {
+    return true
+} else {
+    return false
+}
